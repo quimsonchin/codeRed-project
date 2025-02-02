c4ed5d78c4e5cf55a8f8799c7cd9401c8c3f8653
Selma Bédoui <selmabeds@gmail.com>
2025-02-01 22:12:36 -0500

    Update Icy-Cat.py

--- Icy-Cat.py
+++ Icy-Cat.py
@@ -1,7 +1,14 @@
+# Window set-up
 # Import GUI
 import tkinter as tk
 from tkinter import ttk
 
-#
+#Create main window
+game = tk.Tk()
+
+# Windows Properties
+game.title("Icy Cat")
+game.geometry("1000x500")
+    
 #Ensure window stays open & interactive
-window.mainloop()
+game.mainloop()
